dd8cfe2c4a717b5d8030e0d05071859e93d14842
Peter André Johansen <peterandrejohansen@gmail.com>
2016-04-16 02:08:02 +0200

    Polished ValueChecker

--- src/main/java/com/actram/configent/ValueChecker.java
+++ src/main/java/com/actram/configent/ValueChecker.java
@@ -5,21 +5,23 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Checks the value of an object to ensure safe casting at a later point. All
- * checking methods are chainable and throw a {@link IllegalValueException} if
+ * checking methods are chainable and throw an {@link IllegalValueException} if
  * the value is invalid.
  * <p>
  * In most cases you can perform checks using the methods that start with
  * {@code require}, but if you need to perform a custom check, you can use:
  * {@link #check(Consumer)} or the methods that start with {@code checkAgainst}
- * to check against collections.
+ * to check against collections. Values that are {@code null} will automatically
+ * be handled in custom checks as long as you use these methods, which means
+ * {@link NullPointerException}s will never occur.
  * <p>
- * Values that are {@code null} will automatically be handled, which means
- * {@link NullPointerException}s will never occur. By default, {@code null}
- * -values are not allowed and will throw a {@link IllegalValueException} if
- * checked. This can be changed by calling {@link #allowNull()}.
+ * By default, values that are {@code null} are not allowed and will throw an
+ * {@link IllegalValueException} if checked. This can be changed by calling
+ * {@link #allowNull()}.
  * <p>
  * An example of a custom check to ensure the value is an even integer:
  * 
@@ -27,8 +29,8 @@ import java.util.function.Consumer;
  * 	check(value -> {
  * 		requireInteger();
  * 		final int n = (Integer) value;
- * 		if (n % != 0) {
- * 			fail("value must be an even integer");
+ * 		if (n % 2 != 0) {
+ * 			fail("value must be an even integer: " + n);
  * 		}
  * 	});
  * </pre>
@@ -42,7 +44,7 @@ public class ValueChecker<T> {
 	/**
 	 * Specifies that the value can be {@code null}.
 	 */
-	public ValueChecker<T> allowNull() {
+	public final ValueChecker<T> allowNull() {
 		if (nullAllowed) {
 			throw new IllegalStateException("null values are already allowed");
 		}
@@ -59,7 +61,7 @@ public class ValueChecker<T> {
 	 * @function the function that checks the value
 	 * @throws IllegalValueException if the value is invalid
 	 */
-	public ValueChecker<T> check(Consumer<T> function) {
+	public final ValueChecker<T> check(Consumer<T> function) {
 		Objects.requireNonNull(function, "check function cannot be null");
 		if (value != null) {
 			function.accept(value);
@@ -81,7 +83,7 @@ public class ValueChecker<T> {
 	 *            value in the array (second type)
 	 */
 	@SuppressWarnings("unchecked")
-	public <R> ValueChecker<?> checkAgainst(BiConsumer<T, R> function, R... values) {
+	public final <R> ValueChecker<?> checkAgainst(BiConsumer<T, R> function, R... values) {
 		Objects.requireNonNull(values, "list of values cannot be null");
 		return checkAgainst(Arrays.asList(values), function);
 	}
@@ -99,7 +101,7 @@ public class ValueChecker<T> {
 	 * @param function the function to check the value (first type) against a
 	 *            value in the collection (second type)
 	 */
-	public <R> ValueChecker<T> checkAgainst(Collection<R> values, BiConsumer<T, R> function) {
+	public final <R> ValueChecker<T> checkAgainst(Collection<R> values, BiConsumer<T, R> function) {
 		Objects.requireNonNull(values, "list of values cannot be null");
 		Objects.requireNonNull(function, "check function cannot be null");
 		if (values.size() == 0) {
@@ -114,7 +116,9 @@ public class ValueChecker<T> {
 	}
 
 	/**
-	 * Throws an {@link IllegalValueException} with the given message.
+	 * Throws an {@link IllegalValueException} with the given message. The
+	 * exception message should, if possible, specify specifically why the value
+	 * failed a check.
 	 * 
 	 * @param message the exception message
 	 * @throws IllegalValueException
@@ -127,15 +131,29 @@ public class ValueChecker<T> {
 	/**
 	 * Calls {@link #fail(String)} with the specified formatted message.
 	 */
-	public void fail(String messageFormat, Object... args) {
+	public final void fail(String messageFormat, Object... args) {
 		Objects.requireNonNull(args, "exception message format arguments cannot be null");
 		throw new IllegalValueException(String.format(messageFormat, args));
 	}
 
 	/**
+	 * Convenience method to require that the value is a {@link boolean}.
+	 */
+	public final ValueChecker<T> requireBoolean() {
+		return requireType(Boolean.class);
+	}
+
+	/**
+	 * Convenience method to require that the value is a {@link char}.
+	 */
+	public final ValueChecker<T> requireCharacter() {
+		return requireType(Character.class);
+	}
+
+	/**
 	 * Requires that the value is a {@link Comparable}.
 	 */
-	public ValueChecker<T> requireComparable() {
+	public final ValueChecker<T> requireComparable() {
 		return check(value -> {
 			if (!(value instanceof Comparable<?>)) {
 				fail("value must be comparable");
@@ -144,43 +162,67 @@ public class ValueChecker<T> {
 	}
 
 	/**
-	 * Requires that the value is in the given collection.
+	 * Convenience method to require that the value is a {@link double}.
 	 */
-	public ValueChecker<T> requireIn(Collection<T> values) {
+	public final ValueChecker<T> requireDouble() {
+		return requireType(Double.class);
+	}
+
+	/**
+	 * Calls {@link #requireIn(Collection, String)} with a generic exception
+	 * message.
+	 */
+	public final ValueChecker<T> requireIn(Collection<T> values) {
+		return requireIn(values, "value must be in the list");
+	}
+
+	/**
+	 * Requires that the value is in the given collection.
+	 * 
+	 * @param exceptionMessage the specific exception message to use if the
+	 *            value is not found in the collection
+	 */
+	public final ValueChecker<T> requireIn(Collection<T> values, String exceptionMessage) {
 		return check(value -> {
 			if (!values.contains(value)) {
-				fail("value must be in the list");
+				fail(exceptionMessage);
 			}
 		});
 	}
 
 	/**
 	 * Requires that the value is in the given array.
+	 * 
+	 * @param exceptionMessage the specific exception message to use if the
+	 *            value is not found in the array
 	 */
-	@SuppressWarnings("unchecked")
-	public ValueChecker<T> requireIn(T... values) {
+	@SafeVarargs
+	public final ValueChecker<T> requireIn(String exceptionMessage, T... values) {
 		Objects.requireNonNull(values, "list of values cannot be null");
-		return requireNot(Arrays.asList(values));
+		return requireIn(Arrays.asList(values), exceptionMessage);
 	}
 
 	/**
-	 * Explicitly requires that the value is an {@code int}.
+	 * Calls {@link #requireIn(Object[], String)} with a generic exception
+	 * message.
 	 */
-	private ValueChecker<T> requireInteger() {
-		return check(value -> {
-			try {
-				Integer.parseInt(value.toString());
-			} catch (NumberFormatException e) {
-				fail("value must be an integer");
-			}
-		});
+	@SafeVarargs
+	public final ValueChecker<T> requireIn(T... values) {
+		return this.requireIn("value must be in the array", values);
+	}
+
+	/**
+	 * Convenience method to require that the value is an {@link int}.
+	 */
+	public final ValueChecker<T> requireInteger() {
+		return requireType(Integer.class);
 	}
 
 	/**
 	 * Requires that the value is an integer between the specified range
 	 * (inclusive on both ends).
 	 */
-	public ValueChecker<T> requireIntegerBetween(int min, int max) {
+	public final ValueChecker<T> requireIntegerBetween(int min, int max) {
 		return requireInteger().check(value -> {
 			if ((Integer) value < min || (Integer) value > max) {
 				fail("value must be an integer between or equal %s and %s, currently: %s", min, max, value);
@@ -192,7 +234,7 @@ public class ValueChecker<T> {
 	 * Requires that the value is an integer less than or equal to the specified
 	 * value.
 	 */
-	public ValueChecker<T> requireIntegerMax(int max) {
+	public final ValueChecker<T> requireIntegerMax(int max) {
 		return requireInteger().check(value -> {
 			if ((Integer) value > max) {
 				fail("value must be an integer less than or equal to %s, currently: $s", max, value);
@@ -204,7 +246,7 @@ public class ValueChecker<T> {
 	 * Requires that the value is an integer greater than or equal to the
 	 * specified value.
 	 */
-	public ValueChecker<T> requireIntegerMin(int min) {
+	public final ValueChecker<T> requireIntegerMin(int min) {
 		return requireInteger().check(value -> {
 			if ((Integer) value < min) {
 				fail("value must be an integer greater than or equal to %s, currently: %s", min, value);
@@ -213,22 +255,24 @@ public class ValueChecker<T> {
 	}
 
 	/**
-	 * Requires that the value is a string and matches the given regex.
+	 * Calls {@link #requireMatch(String, String)} with a generic exception
+	 * message.
 	 */
-	public ValueChecker<T> requireMatch(String regex) {
+	public final ValueChecker<T> requireMatch(String regex) {
 		return requireMatch(regex, String.format("value must match %s, currently: %s", regex, value));
 	}
 
 	/**
-	 * This method does the same as {@link #requireMatch(String)}, but lets you
-	 * specify a more specific exception message.
+	 * Requires that the value is a string and matches the given regex.
+	 * 
+	 * @param exceptionMessage the specific exception message to use if the
+	 *            value does not match the regex
 	 */
-	public ValueChecker<T> requireMatch(String regex, String customExceptionMessage) {
+	public final ValueChecker<T> requireMatch(String regex, String exceptionMessage) {
 		Objects.requireNonNull(regex, "regex cannot be null");
-		Objects.requireNonNull(customExceptionMessage, "custom exception message cannot be null");
 		return check(value -> {
 			if (!((String) value).matches(regex)) {
-				fail(customExceptionMessage);
+				fail(exceptionMessage);
 			}
 		});
 	}
@@ -236,7 +280,7 @@ public class ValueChecker<T> {
 	/**
 	 * Requires that the value is an integer greater than zero.
 	 */
-	public ValueChecker<T> requireNaturalInteger() {
+	public final ValueChecker<T> requireNaturalInteger() {
 		return requireInteger().check(value -> {
 			if ((Integer) value > 0) {
 				fail("value must be an integer greater than zero, currently: %s", value);
@@ -247,7 +291,7 @@ public class ValueChecker<T> {
 	/**
 	 * Requires that the value is a negative integer.
 	 */
-	public ValueChecker<T> requireNegativeInteger() {
+	public final ValueChecker<T> requireNegativeInteger() {
 		return requireInteger().check(value -> {
 			if ((Integer) value < 0) {
 				fail("value must be a negative integer, currently: %s", value);
@@ -258,7 +302,7 @@ public class ValueChecker<T> {
 	/**
 	 * Requires that the value is a string with a length greater than zero.
 	 */
-	public ValueChecker<T> requireNonEmptyString() {
+	public final ValueChecker<T> requireNonEmptyString() {
 		return requireString().check(value -> {
 			if (((String) value).isEmpty()) {
 				fail("value must be a non-empty string");
@@ -267,29 +311,77 @@ public class ValueChecker<T> {
 	}
 
 	/**
-	 * Requires that the value is not in the given collection.
+	 * Calls {@link #requireNot(Collection, String)} with a generic exception
+	 * message.
 	 */
-	public ValueChecker<T> requireNot(Collection<T> values) {
+	public final ValueChecker<T> requireNot(Collection<T> values) {
+		return this.requireNot(values, "value cannot be in the list");
+	}
+
+	/**
+	 * Requires that the value is not in the given collection.
+	 * 
+	 * @param exceptionMessage the specific exception message to use if the
+	 *            value is found in the collection
+	 */
+	public final ValueChecker<T> requireNot(Collection<T> values, String exceptionMessage) {
 		Objects.requireNonNull(values, "list of values cannot be null");
 		return check(value -> {
 			if (values.contains(value)) {
-				fail("value cannot be in the list");
+				fail(exceptionMessage);
 			}
 		});
 	}
 
 	/**
 	 * Requires that the value is not in the given array.
+	 * 
+	 * @param exceptionMessage the specific exception message to use if the
+	 *            value is found in the array
 	 */
-	public ValueChecker<T> requireNot(Object... values) {
+	@SafeVarargs
+	public final ValueChecker<T> requireNot(String exceptionMessage, T... values) {
 		Objects.requireNonNull(values, "list of values cannot be null");
-		return requireNot(Arrays.asList(values));
+		return requireNot(Arrays.asList(values), exceptionMessage);
+	}
+
+	/**
+	 * Calls {@link #requireNot(String, Object...)} with a generic exception
+	 * message.
+	 */
+	@SafeVarargs
+	public final ValueChecker<T> requireNot(T... values) {
+		Objects.requireNonNull(values, "list of values cannot be null");
+		return requireNot(Arrays.asList(values), "value cannot be in the array");
+	}
+
+	/**
+	 * Calls {@link #requirePassTest(Predicate, String)} with a generic
+	 * exception message.
+	 */
+	public final ValueChecker<T> requirePassTest(Predicate<T> predicate) {
+		return this.requirePassTest(predicate, "value must pass predicate");
+	}
+
+	/**
+	 * Requires that the value passes the given predicate.
+	 * 
+	 * @param exceptionMessage the specific exception message to use if the
+	 *            value fails the test
+	 */
+	public final ValueChecker<T> requirePassTest(Predicate<T> predicate, String exceptionMessage) {
+		Objects.requireNonNull(predicate, "predicate cannot be null");
+		return check(value -> {
+			if (!predicate.test(value)) {
+				fail(exceptionMessage);
+			}
+		});
 	}
 
 	/**
 	 * Requires that the value is an integer greater than or equal to zero.
 	 */
-	public ValueChecker<T> requirePositiveInteger() {
+	public final ValueChecker<T> requirePositiveInteger() {
 		return requireInteger().check(value -> {
 			if ((Integer) value >= 0) {
 				fail("value must be positive integer, currently: %s", value);
@@ -298,16 +390,16 @@ public class ValueChecker<T> {
 	}
 
 	/**
-	 * Explicitly requires that the value is a {@link String}.
+	 * Convenience method to require that the value is a {@link String}.
 	 */
-	private ValueChecker<T> requireString() {
+	public final ValueChecker<T> requireString() {
 		return requireType(String.class);
 	}
 
 	/**
 	 * Requires that the value is assignable from every class specified.
 	 */
-	public ValueChecker<T> requireType(Class<?>... types) {
+	public final ValueChecker<T> requireType(Class<?>... types) {
 		Objects.requireNonNull(types, "list of types cannot be null");
 		return requireType(Arrays.asList(types));
 	}
@@ -316,7 +408,7 @@ public class ValueChecker<T> {
 	 * Requires that the value is assignable from every class in the given
 	 * collection.
 	 */
-	public ValueChecker<T> requireType(Collection<Class<?>> types) {
+	public final ValueChecker<T> requireType(Collection<Class<?>> types) {
 		Objects.requireNonNull(types, "list of types cannot be null");
 		for (Class<?> type : types) {
 			Objects.requireNonNull(type, "a class cannot be null");
@@ -329,9 +421,9 @@ public class ValueChecker<T> {
 	}
 
 	/**
-	 * Resets the checker and sets the new value.
+	 * Resets the checker and sets the new value to perform checks on.
 	 */
-	public void setValue(T value) {
+	public final void setValue(T value) {
 		nullAllowed = false;
 		this.value = value;
 	}
